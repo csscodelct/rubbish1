9918852914f3320f794c63a822b5cca5465bcf60
luocting <luocting@163.com>
2020-12-31 15:34:33 +0800

    first commit

---
+++ pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    categoryData: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // let _this = this
+    // wx.request({
+    //   url: 'http://apis.juhe.cn/rubbish/category',
+    //   data: {
+    //     key: '0cf88a5762c4a25eaed2eec540460b08'
+    //   },
+    //   success(res) {
+    //     _this.setData({
+    //       categoryData: res.data.result
+    //     })
+    //   }
+    // })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
